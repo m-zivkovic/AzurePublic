ede01463a35843fc902054223c425d578abe99a0
milos.zivkovic <milos@itnetworks.pro>
2024-07-16 16:56:10 +0200

    Create powerbi_api_reports_admin.py

---
+++ PowerBI API/powerbi_api_reports_admin.py
@@ -0,0 +1,32 @@
+import requests
+import msal
+
+
+APP_ID = ''
+TENANT_ID = ''
+SECRET_VALUE = ''
+WORKSPACE_ID = ''
+authority = f'https://login.microsoftonline.com/{TENANT_ID}'
+scopes = ["https://analysis.windows.net/powerbi/api/.default"]
+
+app = msal.ConfidentialClientApplication(APP_ID, authority=authority, client_credential=SECRET_VALUE)
+
+result = None
+result = app.acquire_token_for_client(scopes=scopes)
+
+
+#print(result)
+
+if not "access_token" in result:
+    print(result.get("error"))
+    print(result.get("error_description"))
+    print(result.get("correlation_id"))
+    raise Exception("Failed to get access token")
+
+
+url_admin_reports_list = "https://api.powerbi.com/v1.0/myorg/admin/reports"
+headers = {
+    'Authorization': 'Bearer ' + result['access_token']
+}
+response = requests.get(url_admin_reports_list, headers=headers)
+print(response.json())
